e3bfffeb45e68a2bf5ec0d0731f7be2dcc0d41b9
Rafael Correa <scriptsengineer@gmail.com>
2023-09-23 17:53:15 -0300

    Add base steam multiplayer peer

--- godotsteampeer/steam_multiplayer_peer.cpp
+++ godotsteampeer/steam_multiplayer_peer.cpp
@@ -1,6 +1,100 @@
-#include "steam_multiplayer_peer.h"
 #include <godot_cpp/core/class_db.hpp>
 
-void godot::SteamMultiplayerPeer::_bind_methods()
+#include "steam_multiplayer_peer.h"
+
+using namespace godot;
+
+Error SteamMultiplayerPeer::_get_packet(const uint8_t **r_buffer, int32_t *r_buffer_size)
+{
+    return Error();
+}
+
+Error SteamMultiplayerPeer::_put_packet(const uint8_t *p_buffer, int32_t p_buffer_size)
+{
+    return Error();
+}
+
+int32_t SteamMultiplayerPeer::_get_available_packet_count() const
+{
+    return 0;
+}
+
+int32_t SteamMultiplayerPeer::_get_max_packet_size() const
+{
+    return 0;
+}
+
+// PackedByteArray SteamMultiplayerPeer::_get_packet_script()
+// {
+//     return PackedByteArray();
+// }
+
+// Error SteamMultiplayerPeer::_put_packet_script(const PackedByteArray &p_buffer)
+// {
+//     return Error();
+// }
+
+int32_t SteamMultiplayerPeer::_get_packet_channel() const
+{
+    return 0;
+}
+
+MultiplayerPeer::TransferMode SteamMultiplayerPeer::_get_packet_mode() const
+{
+    return MultiplayerPeer::TransferMode();
+}
+
+// void SteamMultiplayerPeer::_set_transfer_channel(int32_t p_channel) {}
+
+// int32_t SteamMultiplayerPeer::_get_transfer_channel() const
+// {
+//     return 0;
+// }
+
+// void SteamMultiplayerPeer::_set_transfer_mode(
+//     MultiplayerPeer::TransferMode p_mode) {}
+
+void SteamMultiplayerPeer::_set_target_peer(int32_t p_peer) {}
+
+int32_t SteamMultiplayerPeer::_get_packet_peer() const
+{
+    return 0;
+}
+
+bool SteamMultiplayerPeer::_is_server() const
+{
+    return false;
+}
+
+void SteamMultiplayerPeer::_poll() {}
+
+void SteamMultiplayerPeer::_close() {}
+
+void SteamMultiplayerPeer::_disconnect_peer(int32_t p_peer,
+                                                   bool p_force) {}
+
+int32_t SteamMultiplayerPeer::_get_unique_id() const
+{
+    return 0;
+}
+
+// void SteamMultiplayerPeer::_set_refuse_new_connections(bool p_enable) {}
+
+// bool SteamMultiplayerPeer::_is_refusing_new_connections() const
+// {
+//     return false;
+// }
+
+bool SteamMultiplayerPeer::_is_server_relay_supported() const
+{
+    return false;
+}
+
+MultiplayerPeer::ConnectionStatus SteamMultiplayerPeer::_get_connection_status() const
+{
+    return MultiplayerPeer::ConnectionStatus();
+}
+
+void SteamMultiplayerPeer::_bind_methods()
 {
 }
--- godotsteampeer/steam_multiplayer_peer.h
+++ godotsteampeer/steam_multiplayer_peer.h
@@ -6,14 +6,37 @@
 namespace godot {
 
 class SteamMultiplayerPeer : MultiplayerPeerExtension {
-  GDCLASS(SteamMultiplayerPeer, MultiplayerPeerExtension);
+  GDCLASS(SteamMultiplayerPeer, MultiplayerPeerExtension)
 
-  public:
-    Error _get_packet(const uint8_t * *r_buffer, int32_t *r_buffer_size);
+public:
+  SteamMultiplayerPeer();
+  ~SteamMultiplayerPeer();
+  Error _get_packet(const uint8_t **r_buffer, int32_t *r_buffer_size) override;
+  Error _put_packet(const uint8_t *p_buffer, int32_t p_buffer_size) override;
+  int32_t _get_available_packet_count() const override;
+  int32_t _get_max_packet_size() const override;
+  // PackedByteArray _get_packet_script();
+  // Error _put_packet_script(const PackedByteArray &p_buffer);
+  int32_t _get_packet_channel() const;
+  MultiplayerPeer::TransferMode _get_packet_mode() const;
+  // void _set_transfer_channel(int32_t p_channel) override;
+  // int32_t _get_transfer_channel() const;
+  // void _set_transfer_mode(MultiplayerPeer::TransferMode p_mode) override;
+  // MultiplayerPeer::TransferMode _get_transfer_mode() const override;
+  void _set_target_peer(int32_t p_peer) override;
+  int32_t _get_packet_peer() const override;
+  bool _is_server() const override;
+  void _poll() override;
+  void _close() override;
+  void _disconnect_peer(int32_t p_peer, bool p_force) override;
+  int32_t _get_unique_id() const override;
+  // void _set_refuse_new_connections(bool p_enable) override;
+  // bool _is_refusing_new_connections() const override;
+  bool _is_server_relay_supported() const override;
+  MultiplayerPeer::ConnectionStatus _get_connection_status() const override;
 
-  protected:
-    static void _bind_methods();
-    
+protected:
+  static void _bind_methods();
 };
 
 } // namespace godot
